b8a0d6da27083c4e02cb77d28a24e8ad547681c1
Asha <asha17@brown.edu>
2019-07-27 11:01:31 -0700

    character card adn list functional

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,13 @@
 import React from 'react'
 
-export default function CharacterCard ({ /* add props */ }) {
-  return (<span>todo: location</span>
-}
+export default function CharacterCard (props) {
+  return (
+  <span>
+<div>
+  <img src={props.image} alt={props.name}/>
+  <h2>{props.name}</h2>
+  <h3>{`${props.species} ${props.status}`}</h3>
+</div>
+  </span>
+  );
+};
