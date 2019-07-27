7c9c2dc045159439cd9e8e52aed09efd130aaa2b
Asha <asha17@brown.edu>
2019-07-27 11:36:05 -0700

    MVP Completed

--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -14,10 +14,10 @@ const Nav = props => (
 
 const createLabel = (iconName, labelText) => <span><Icon name={iconName} />{labelText}</span>
 
-const welcomeLabel = createLabel("home", "Home Page")
-const characterLabel = createLabel("users", "Characters")
-const locationLabel = createLabel("places", "Locations")
-const episodeLabel = createLabel("series", "Episodes")
+const welcomeLabel = createLabel("home", " Home Page")
+const characterLabel = createLabel("users", " Characters")
+const locationLabel = createLabel("location arrow", " Locations")
+const episodeLabel = createLabel("tv", " Episodes")
 
 const panes = [
   { menuItem: <Menu.Item key='home' as={Nav} to={`/`} content={welcomeLabel} /> },
